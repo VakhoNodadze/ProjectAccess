5574f511dea94fb171d5903f5eb359c8895f5cbe
VakhoNodadze <vaxonodadze@gmail.com>
2020-12-13 22:38:11 +0400

    safeguard for edit page

--- src/pages/EditUser.js
+++ src/pages/EditUser.js
@@ -8,15 +8,24 @@ import UserData from '../components/UserData';
 const EditUser = ({onThemeChange, userObj, isDark, theme}) => {
 
   return (
-    <Flex direction="column" full>
-      <Grid style={{padding: "4rem 4rem 0 4rem", maxWidth: '100%'}}>
-        <Grid.Item xs={1} />
-        <Grid.Item xs={6}>
-          <h1 style={{color: theme.color.text, fontSize: theme.fontSizes.h1}}>User Setup</h1>
-        </Grid.Item>
-      </Grid>
-      <UserData userObj={userObj} onThemeChange={onThemeChange} isDark={isDark} />
-    </Flex>
+    <>
+      {
+        userObj ? 
+          <Flex direction="column" full>
+            <Grid style={{padding: "4rem 4rem 0 4rem", maxWidth: '100%'}}>
+              <Grid.Item xs={1} />
+              <Grid.Item xs={6}>
+                <h1 style={{color: theme.color.text, fontSize: theme.fontSizes.h1}}>User Setup</h1>
+              </Grid.Item>
+            </Grid>
+            <UserData userObj={userObj} onThemeChange={onThemeChange} isDark={isDark} />
+          </Flex>
+          : <h1 
+            style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+            You haven't chosen any users yet!
+          </h1>
+      }
+    </>
   );
 };
 
